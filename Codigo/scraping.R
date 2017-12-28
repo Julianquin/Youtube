2707d4b46c8c0d086c53f629e5ef97f7568a930d
julianquin <julian.alberto.quintero@correounivalle.edu.co>
2017-12-28 17:23:45 -0500

    Web scraping

--- Codigo/scraping.R
+++ Codigo/scraping.R
@@ -59,3 +59,170 @@ runtime_data<-as.numeric(runtime_data)
 
 #Let's have another look at the runtime data
 head(runtime_data)
+
+
+#Using CSS selectors to scrap the Movie genre section
+genre_data_html <- html_nodes(webpage,'.genre')
+
+#Converting the genre data to text
+genre_data <- html_text(genre_data_html)
+
+#Let's have a look at the runtime
+head(genre_data)
+
+#Data-Preprocessing: removing \n
+genre_data<-gsub("\n","",genre_data)
+
+#Data-Preprocessing: removing excess spaces
+genre_data<-gsub(" ","",genre_data)
+
+#taking only the first genre of each movie
+genre_data<-gsub(",.*","",genre_data)
+
+#Convering each genre from text to factor
+genre_data<-as.factor(genre_data)
+
+#Let's have another look at the genre data
+head(genre_data)
+
+#Using CSS selectors to scrap the IMDB rating section
+rating_data_html <- html_nodes(webpage,'.ratings-imdb-rating strong')
+
+#Converting the ratings data to text
+rating_data <- html_text(rating_data_html)
+
+#Let's have a look at the ratings
+head(rating_data)
+
+#Data-Preprocessing: converting ratings to numerical
+rating_data<-as.numeric(rating_data)
+
+#Let's have another look at the ratings data
+head(rating_data)
+
+#Using CSS selectors to scrap the votes section
+votes_data_html <- html_nodes(webpage,'.sort-num_votes-visible span:nth-child(2)')
+
+#Converting the votes data to text
+votes_data <- html_text(votes_data_html)
+
+#Let's have a look at the votes data
+head(votes_data)
+
+#Data-Preprocessing: removing commas
+votes_data<-gsub(",","",votes_data)
+
+#Data-Preprocessing: converting votes to numerical
+votes_data<-as.numeric(votes_data)
+
+#Let's have another look at the votes data
+head(votes_data)
+
+
+
+
+
+#Using CSS selectors to scrap the directors section
+directors_data_html <- html_nodes(webpage,'.text-muted+ p a:nth-child(1)')
+
+#Converting the directors data to text
+directors_data <- html_text(directors_data_html)
+
+#Let's have a look at the directors data
+head(directors_data)
+
+#Data-Preprocessing: converting directors data into factors
+directors_data<-as.factor(directors_data)
+
+#Using CSS selectors to scrap the actors section
+actors_data_html <- html_nodes(webpage,'.lister-item-content .ghost+ a')
+
+#Converting the gross actors data to text
+actors_data <- html_text(actors_data_html)
+
+#Let's have a look at the actors data
+head(actors_data)
+
+#Data-Preprocessing: converting actors data into factors
+actors_data<-as.factor(actors_data)
+
+#Let's have a look at the actors data
+head(actors_data)
+
+#Using CSS selectors to scrap the metascore section
+metascore_data_html <- html_nodes(webpage,'.metascore')
+
+#Converting the runtime data to text
+metascore_data <- html_text(metascore_data_html)
+
+#Let's have a look at the metascore 
+head(metascore_data)
+
+#Data-Preprocessing: removing extra space in metascore
+metascore_data<-gsub(" ","",metascore_data)
+
+#Lets check the length of metascore data
+length(metascore_data)
+
+
+for (i in c(46,52)){
+  a<-metascore_data[1:(i-1)]
+  b<-metascore_data[i:length(metascore_data)]
+  metascore_data<-append(a,list("NA"))
+  metascore_data<-append(metascore_data,b)
+}
+
+#Data-Preprocessing: converting metascore to numerical
+metascore_data<-as.numeric(metascore_data)
+
+#Let's have another look at length of the metascore data
+
+length(metascore_data)
+
+
+#Let's look at summary statistics
+summary(metascore_data)
+
+
+#Using CSS selectors to scrap the gross revenue section
+gross_data_html <- html_nodes(webpage,'.ghost~ .text-muted+ span')
+
+#Converting the gross revenue data to text
+gross_data <- html_text(gross_data_html)
+
+#Let's have a look at the votes data
+head(gross_data)
+
+
+#Data-Preprocessing: removing '$' and 'M' signs
+gross_data<-gsub("M","",gross_data)
+
+gross_data<-substring(gross_data,2,6)
+
+#Let's check the length of gross data
+length(gross_data)
+
+[1] 86
+
+#Filling missing entries with NA
+for (i in c(17,39,49,52,57,64,66,73,76,77,80,87,88,89)){
+  
+  a<-gross_data[1:(i-1)]
+  
+  b<-gross_data[i:length(gross_data)]
+  
+  gross_data<-append(a,list("NA"))
+  
+  gross_data<-append(gross_data,b)
+  
+}
+
+#Data-Preprocessing: converting gross to numerical
+gross_data<-as.numeric(gross_data)
+
+#Let's have another look at the length of gross data
+length(gross_data)
+
+[1] 100
+
+summary(gross_data)
