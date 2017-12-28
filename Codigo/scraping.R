8838aa964864bb93f9a04d646f8d7b6a91fdb576
julianquin <julian.alberto.quintero@correounivalle.edu.co>
2017-12-28 17:01:57 -0500

    Se recreo el codigo haciendo web scraping

---
+++ Codigo/scraping.R
@@ -0,0 +1,61 @@
+library('rvest')
+
+#Specifying the url for desired website to be scrapped
+url <- 'http://www.imdb.com/search/title?count=100&release_date=2016,2016&title_type=feature'
+
+#Reading the HTML code from the website
+webpage <- read_html(url)
+
+rank_data_html <- html_nodes(webpage,'.text-primary')
+
+#Converting the ranking data to text
+rank_data <- html_text(rank_data_html)
+
+#Let's have a look at the rankings
+head(rank_data)
+
+#Data-Preprocessing: Converting rankings to numerical
+rank_data<-as.numeric(rank_data)
+
+#Let's have another look at the rankings
+head(rank_data)
+
+title_data_html <- html_nodes(webpage,'.lister-item-header a')
+
+#Converting the title data to text
+title_data <- html_text(title_data_html)
+
+#Let's have a look at the title
+head(title_data)
+
+#Using CSS selectors to scrap the description section
+description_data_html <- html_nodes(webpage,'.ratings-bar+ .text-muted')
+
+#Converting the description data to text
+description_data <- html_text(description_data_html)
+
+#Let's have a look at the description data
+head(description_data)
+
+#Data-Preprocessing: removing '\n'
+description_data<-gsub("\n","",description_data)
+
+#Let's have another look at the description data 
+head(description_data)
+
+#Using CSS selectors to scrap the Movie runtime section
+runtime_data_html <- html_nodes(webpage,'.text-muted .runtime')
+
+#Converting the runtime data to text
+runtime_data <- html_text(runtime_data_html)
+
+#Let's have a look at the runtime
+head(runtime_data)
+
+#Data-Preprocessing: removing mins and converting it to numerical
+
+runtime_data<-gsub(" min","",runtime_data)
+runtime_data<-as.numeric(runtime_data)
+
+#Let's have another look at the runtime data
+head(runtime_data)
